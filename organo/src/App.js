a830ab00fe74d2aadf22c92288513a91a50a5c38
Mariah Silveira <mariah.silveira@uerj.br>
2024-11-27 16:21:13 -0300

    first commit

---
+++ organo/src/App.js
@@ -0,0 +1,13 @@
+import Banner from './componentes/Banner';
+import CampoTexto from './componentes/CampoTexto';
+
+function App() {
+  return (
+    <div className="App">
+      <Banner /> {/* parece uma tag html mas foi criado no arquivo Banner.js */} 
+      <CampoTexto />
+    </div>
+  );
+}
+
+export default App;
---
+++ organo/src/componentes/CampoTexto/index.js
@@ -0,0 +1,11 @@
+const CampoTexto = () => {
+    return (
+        <div className="campo-texto">
+            <label>Nome</label>
+            <input></input>
+        </div>
+    )
+}
+
+export default CampoTexto
+
