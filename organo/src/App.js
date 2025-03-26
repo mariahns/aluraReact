fbbc82a6f1b87d0385ca551cb6a4bc8bcdeaa042
Mariah Silveira <mariah.silveira@uerj.br>
2025-03-26 10:00:40 -0300

    segundo curso - terceira parte

--- organo/src/App.js
+++ organo/src/App.js
@@ -6,43 +6,36 @@ import Time from "./componentes/Time";
 
 function App() {
 
-  const times = [
+  const [times, setTimes] = useState ([
     {
-      nome: 'Programação',
-      corPrimaria: '#D9F7E9',
-      corSecundaria: '#57C278'
+      nome: 'Programação',      
+      cor: '#57C278'
     },
     {
-      nome: 'Front-End',
-      corPrimaria: '#E8F8FF',
-      corSecundaria: '#82CFFA'
+      nome: 'Front-End',      
+      cor: '#82CFFA'
     },
     {
-      nome: 'Data Science',
-      corPrimaria: '#F0F8E2',
-      corSecundaria: '#A6D157'
+      nome: 'Data Science',      
+      cor: '#A6D157'
     },
     {
-      nome: 'Devops',
-      corPrimaria: '#FDE7E8',
-      corSecundaria: '#E06B69'
+      nome: 'Devops',      
+      cor: '#E06B69'
     },
     {
-      nome: 'UX e Design',
-      corPrimaria: '#FAE9F5',
-      corSecundaria: '#DB6EBF'
+      nome: 'UX e Design',      
+      cor: '#DB6EBF'
     },
     {
-      nome: 'Mobile',
-      corPrimaria: '#FFF5D9',
-      corSecundaria: '#FFBA05'
+      nome: 'Mobile',      
+      cor: '#FFBA05'
     },
     {
-      nome: 'Inovação e Gestão',
-      corPrimaria: '#FFEEDF',
-      corSecundaria: '#FF8A29'
+      nome: 'Inovação e Gestão',      
+      cor: '#FF8A29'
     },
-  ]
+  ]);
 
   const inicial = [
     {
@@ -197,6 +190,15 @@ function App() {
     console.log('deletando');
   }
 
+  function mudarCordoTime(cor, nome) {
+    setTimes( times.map ( time => {
+        if (time.nome === nome) {
+          time.cor = cor; 
+        }
+        return time;
+    }))   
+  }   
+
 
   return (
     <div>
@@ -206,6 +208,7 @@ function App() {
         <h1>Minha organização</h1>
         {times.map((time, indice) => 
         <Time 
+          mudarCor={mudarCordoTime}
           key={indice} 
           time={time} 
           colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)} 
--- organo/src/componentes/Time/index.js
+++ organo/src/componentes/Time/index.js
@@ -1,15 +1,16 @@
 import Colaborador from '../Colaborador'
+import hexToRgba from 'hex-to-rgba';
 import './time.css'
 
-const Time = ({ time, colaboradores, aoDeletar }) => {
+const Time = ({ time, colaboradores, aoDeletar, mudarCor }) => {
     return (
-        colaboradores.length > 0 && <section className='time' style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: time.corPrimaria }}>
-            <input type='color' value={time.corSecundaria} className='input-cor' />
-            <h3 style={{ borderColor: time.corSecundaria }}>{time.nome}</h3>
+        colaboradores.length > 0 && <section className='time' style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: hexToRgba(time.cor, '0.2') }}>
+            <input type='color' value={time.cor} className='input-cor' onChange={e => mudarCor(e.target.value, time.nome)} />
+            <h3 style={{ borderColor: time.cor }}>{time.nome}</h3>
             <div className='colaboradores'>
                 {colaboradores.map((colaborador, indice) => {
-                   //console.log(<Colaborador key={indice} colaborador={colaborador} corDeFundo={time.corSecundaria} aoDeletar={() => {}} /> ) 
-                   return <Colaborador key={indice} colaborador={colaborador} corDeFundo={time.corSecundaria} aoDeletar={aoDeletar}/> 
+                   //console.log(<Colaborador key={indice} colaborador={colaborador} corDeFundo={time.cor} aoDeletar={() => {}} /> ) 
+                   return <Colaborador key={indice} colaborador={colaborador} corDeFundo={time.cor} aoDeletar={aoDeletar}/> 
                 })}
             </div>
         </section>
